2cd54da6668abff0d6c05399b83902bbac14f554
Ann-Yuryeva <anutca57@mail.ru>
2021-08-02 23:14:10 +0300

    Создание теста и выделение вспомогательного класса для контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String address;
+  private final String nickname;
+  private final String home;
+  private final String company;
+  private final String mobile;
+  private final String email;
+
+  public ContactData(String firstname, String middlename, String lastname, String address, String nickname, String home, String company, String mobile, String email) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.address = address;
+    this.nickname = nickname;
+    this.home = home;
+    this.company = company;
+    this.mobile = mobile;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
