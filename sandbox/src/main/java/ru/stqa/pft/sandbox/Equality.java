5ba1b629080dac1ed0afb0c7443b5e479deac3f7
Ann-Yuryeva <anutca57@mail.ru>
2021-08-08 23:23:42 +0300

    Пример сравнения

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Equality {
+
+  public static void main(String[] args) {
+    String s1 = "Firefox";
+    String s2 = "Fire" + "fox";
+
+    System.out.println(s1==s2);
+    System.out.println(s1.equals(s2));
+  }
+}
