98c64c090372058f1ae91cd590a21c92f79cd2af
Ann-Yuryeva <anutca57@mail.ru>
2021-08-10 21:55:31 +0300

    Добавлен шаг перехода на HomePage в тестах контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,11 +2,13 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.appmanager.NavigationHelper;
 
 public class ContactModificationTests extends TestBase{
 
   @Test
   public void testContactModification(){
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("John", "Kenny", "Johnas", "LA", "Johny", "123456789", "AT", null,"123456789","test@test.com"),false);
     app.getContactHelper().submitContactModification();
