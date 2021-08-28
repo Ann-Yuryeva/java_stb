3c781f3f0077b079710486af5aed2eddce4a2b60
Ann-Yuryeva <anutca57@mail.ru>
2021-08-28 19:34:04 +0300

    6.7 Сохранение и чтение данных групп контактов в json формате

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,15 +1,18 @@
 package ru.stqa.pft.addressbook.generators;
+
 import com.beust.jcommander.JCommander;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
+import org.openqa.selenium.json.Json;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
 
 
 public class GroupDataGenerator {
@@ -27,7 +30,7 @@ public class GroupDataGenerator {
     GroupDataGenerator generator = new GroupDataGenerator();
     JCommander jCommander = new JCommander(generator);
     try {
-     jCommander.parse(args);
+      jCommander.parse(args);
     } catch (ParameterException ex) {
       jCommander.usage();
       return;
@@ -41,9 +44,19 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
     } else {
       System.out.println("Unrecognized format");
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
